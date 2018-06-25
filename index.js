e4b486eb6ba3886eaa3a9ec6ba03536ec7be45be
Michael Ballard <ballard.michael9@gmail.com>
2018-06-25 21:39:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-let shortGreeting = 
+let shortGreeting = `Welcome,${currentUser.slice(0,1)}`
